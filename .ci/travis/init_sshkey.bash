3bc916b03f0079ba66a9cce23e58edb19e9df75c
Rene Milk <rene.milk@wwu.de>
2017-08-04 17:04:50 +0200

    fixup! fixup! [ci] adds testlog upload

--- .ci/travis/init_sshkey.bash
+++ .ci/travis/init_sshkey.bash
@@ -9,6 +9,7 @@ IV=${2}
 RSAKEY=${3}.rsa.enc
 pushd ${DIR}
 [[ -d ~/.ssh ]] || mkdir -p  ~/.ssh
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 openssl aes-256-cbc -K ${KEY} -iv ${IV} -in ${RSAKEY} -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 popd
